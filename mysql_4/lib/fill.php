93125fb41c914bb3f016c73f07fd6c697c635d72
Andrius Putna <fordnox@gmail.com>
2012-10-20 02:41:34 +0300

    script to scrap products

--- mysql_4/lib/fill.php
+++ mysql_4/lib/fill.php
@@ -8,6 +8,14 @@ ini_set('html_errors', FALSE);
 ini_set('error_log', dirname(__FILE__) . '/php_error.log');
 
 use Goutte\Client;
+use Symfony\Component\BrowserKit\Cookie;
+
+use Guzzle\Http\Message\Response as GuzzleResponse;
+use Guzzle\Http\Client as GuzzleClient;
+use Guzzle\Http\Plugin\MockPlugin;
+use Guzzle\Http\Plugin\HistoryPlugin;
+use Guzzle\Http\Message\Response;
+use Guzzle\Http\Message\PostFile;
 
 class Importer
 {
@@ -36,13 +44,60 @@ class Importer
         }
     }
     
-    public function importProducts()
+    public function importProducts($limit = 1)
     {
-        $crawler = $this->_goutte->request('GET', 'http://www.sportsdirect.com/');
-        //@todo
+        $sql="SELECT m.id, m.slug
+            FROM `manufacturer` m
+            LEFT OUTER JOIN product p ON (m.id = p.manufacturer_id)
+            WHERE p.manufacturer_id IS NULL
+            LIMIT :limit
+        ";
+        $list = R::getAll($sql, array('limit'=>$limit));
+
+        /*
+        $content = file_get_contents('tmp.txt');
+        $plugin = new MockPlugin();
+        $plugin->addResponse(new GuzzleResponse(200, null, $content));
+        $guzzle = new GuzzleClient();
+        $guzzle->getEventDispatcher()->addSubscriber($plugin);
+        $this->_goutte = new Client();
+        $this->_goutte->setClient($guzzle);
+        */
+        
+        foreach ($list as $m) {
+            $url = 'http://www.sportsdirect.com/'.$m['slug'].'/?mppp=true';
+            $crawler = $this->_goutte->request('GET', $url);
+            $nodes = $crawler->filter('.s-productthumbbox');
+            foreach($nodes as $node) {
+
+                $doc = $node->ownerDocument;
+                $doc->preserveWhiteSpace = false;
+                $xpath     = new DOMXPath($doc);
+                
+                $title = trim($xpath->query(".//p[@class='productdescription']", $node)->item(0)->textContent);
+                $image = $xpath->query(".//img", $node)->item(0)->getAttribute('src');
+                $price = $xpath->query(".//span", $node)->item(0)->textContent;
+                $price = preg_replace('/[^\d\.]+/', '', $price);
+                
+                $p = R::dispense('product');
+//                $p->group_id = rand(1, 10);
+                $p->manufacturer_id = $m['id'];
+                $p->title = $title;
+                $p->description = '';
+                $p->price = $price;
+                $p->stock = rand(1, 1000);
+                $p->image = $image;
+                $p->created_at = date('c');
+                $p->updated_at = date('c');
+                R::store($p);
+            }
+            
+            print '.';
+            sleep(4);
+        }
     }
 }
 
 $i = new Importer;
 //$i->importManufacturers();
-//$i->importProducts();
+$i->importProducts(50);
