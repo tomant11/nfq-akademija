1e0e0a52562575c60e9ece400132118eea52bd3d
tomant11 <2balticamber@gmail.com>
2012-10-22 15:21:26 +0300

    Update mysql_4/scripts.php
    pataisyta sintakses klaida

--- mysql_4/scripts.php
+++ mysql_4/scripts.php
@@ -25,63 +25,63 @@ $sql="
     SELECT product.id, product.title, product.price
     FROM product
     ORDER BY price DESC
-    LIMIT 10;
+    LIMIT 10
 ";
 
 $sql="
     SELECT product.id, product.title, product.price
     FROM product
     ORDER BY price ASC
-    LIMIT 10;
+    LIMIT 10
 ";
 
 $sql="
     SELECT product.id, product.title, product.price
     FROM product
     ORDER BY created_at ASC
-    LIMIT 10;
+    LIMIT 10
 ";
 
 $sql="
     SELECT product.id, product.title
     FROM product
-    WHERE group_id is null;
+    WHERE group_id is null
 ";
 
 $sql="
     UPDATE manufacturer
-    SET updated_at="2012.10.21";
+    SET updated_at="2012.10.21"
 ";
 
 $sql="
     UPDATE product
-    SET updated_at="2012.10.21";
+    SET updated_at="2012.10.21"
 ";
 
 $sql="
-    CREATE INDEX pro_ind ON product(title, price);
+    CREATE INDEX pro_ind ON product(title, price)
 ";
 
 $sql="
-    ALTER TABLE product DROP INDEX pro_ind;
+    ALTER TABLE product DROP INDEX pro_ind
 ";
 
 $sql="
     SELECT * 
     FROM product
-    WHERE title LIKE '%lt%';
+    WHERE title LIKE '%lt%'
 ";
 
 $sql="
     SELECT category.*, count(product.id)
     FROM category
         LEFT JOIN product ON (product.group_id=category.id)
-    GROUP BY category.id;
+    GROUP BY category.id
 ";
 
 $sql="
     INSERT INTO category
-    SET title="sportdirect", created_at="2012.10.20", updated_at="2012.10.20";
+    SET title="sportdirect", created_at="2012.10.20", updated_at="2012.10.20"
 ";
 
 $sql="
@@ -90,7 +90,7 @@ $sql="
         LEFT JOIN product ON (product.manufacturer_id=manufacturer.id)
     GROUP BY manufacturer.id
     ORDER BY stock ASC
-    LIMIT 20;
+    LIMIT 20
 ";
 
 $sql="
@@ -99,5 +99,5 @@ $sql="
         LEFT JOIN product ON (manufacturer.id=product.manufacturer_id)
     GROUP BY manufacturer.title
     ORDER BY product_count DESC
-    LIMIT 20;
+    LIMIT 20
 ";
